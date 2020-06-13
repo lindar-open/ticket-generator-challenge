2c427c69637326e06728c863497fc3fa3b4579f0
Przemysław Baj <przemekbaj1@gmail.com>
2020-06-13 20:18:58 +0200

    final version

--- bingo.py
+++ bingo.py
@@ -1,60 +1,11 @@
-from random import randint, choice
-from functions import get_columns, print_field
+from functions import generate_field, print_field
+
+ROWS_NUM = 18
+COLUMNS_NUM = 9
 TICKET_ROWS = 3
-ROWS_LEN = 18
-columns_len = 9
+
 totalField = []
-
-totalFieldReady = False
-while not totalFieldReady: 
-
-    # Generating columns 
-    totalFieldReady = True
-    totalField = []
-    columnsOptions = get_columns(columns_len, ROWS_LEN)
-    
-    for rowIndex in range(0,ROWS_LEN):
-        
-        # Generating rows
-        rowReady = False
-        row = []
-
-        while not rowReady:
-            row = []
-            for index, column in enumerate(columnsOptions):
-                randomChoice = choice(column)
-
-                isTicketLastRow = rowIndex % TICKET_ROWS == TICKET_ROWS-1
                 
-                if(isTicketLastRow):
-                    firstAbove = totalField[rowIndex-1][index]
-                    secondAbove = totalField[rowIndex-2][index]
-                    if (firstAbove == " " and secondAbove == " "):
-                        
-                        columnNumbers = [item for item in column if item.isdigit()]
-                        if(len(columnNumbers)==0):
-                            totalFieldReady = False
-                            break
-                        else: 
-                            randomChoice = choice(columnNumbers)
-
-                row.append(randomChoice)
-
-            else:
-                blanks = [item for item in row if item == " "]
-                if (len(blanks) == 4):
-                    rowReady = True
-                continue
-            break
-
-
-        else: 
-            for index, element in enumerate(row):
-                columnsOptions[index].remove(element)
-
-            totalField.append(row)
-            continue
-        break
-                
+totalField = generate_field(ROWS_NUM,COLUMNS_NUM,TICKET_ROWS)
 
 print_field(totalField, TICKET_ROWS)
--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from random import randint, choice
 
 def get_column_options(_min, _max, blanks_len):
     numbers = [str(i) for i in range(_min,_max+1)]
@@ -11,6 +12,61 @@ def get_columns(_num, rows_len):
         step_end = step_start+9
         result.append(get_column_options(step_start,step_end, rows_len-10))
     return result
+
+
+def generate_field(rows_num, columns_num, ticket_rows_num):
+    totalFieldReady = False
+
+    while not totalFieldReady: 
+        # Generating columns 
+        totalFieldReady = True
+        totalField = []
+        columnsOptions = get_columns(columns_num, rows_num)
+        
+        for rowIndex in range(0,rows_num):
+            # Generating rows
+            rowReady = False
+            row = []
+
+            while not rowReady:
+                row = []
+                for index, column in enumerate(columnsOptions):
+                    randomChoice = choice(column)
+                    isTicketLastRow = rowIndex % ticket_rows_num == ticket_rows_num-1
+
+                    if(isTicketLastRow):
+                        firstAbove = totalField[rowIndex-1][index]
+                        secondAbove = totalField[rowIndex-2][index]
+                        
+                        if (firstAbove == " " and secondAbove == " "):    
+                            columnNumbers = [item for item in column if item.isdigit()]
+                            
+                            if(len(columnNumbers)==0):
+                                totalFieldReady = False
+                                break
+                            else: 
+                                randomChoice = choice(columnNumbers)
+
+                    row.append(randomChoice)
+
+                else:
+                    blanks = [item for item in row if item == " "]
+                    if (len(blanks) == 4):
+                        rowReady = True
+                    continue
+                break
+
+
+            else: 
+                for index, element in enumerate(row):
+                    columnsOptions[index].remove(element)
+
+                totalField.append(row)
+                continue
+            break
+
+    return totalField
+
 
 def print_field(field, ticket_rows):
     _columns_len = len(field[0]) 
