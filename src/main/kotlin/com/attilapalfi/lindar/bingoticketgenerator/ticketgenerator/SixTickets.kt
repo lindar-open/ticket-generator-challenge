95b413c76a579b808e2b3980f8a5c2308e8e2059
Attila Palfi <attila.palfi.92@gmail.com>
2022-07-09 23:00:32 +0200

    deleted a little unused code

--- src/main/kotlin/com/attilapalfi/lindar/bingoticketgenerator/ticketgenerator/SixTickets.kt
+++ src/main/kotlin/com/attilapalfi/lindar/bingoticketgenerator/ticketgenerator/SixTickets.kt
@@ -4,7 +4,6 @@ import java.util.stream.IntStream
 
 class SixTickets {
     val tickets: ArrayList<Ticket> = ArrayList(6)
-    val invalidSpaces = ArrayList<TicketRowColumn>()
 
     init {
         IntStream.range(0, 6).forEach {
@@ -17,6 +16,4 @@ class SixTickets {
         tickets.forEach { stringBuilder.append(it).append("\n") }
         return stringBuilder.toString()
     }
-
-    data class TicketRowColumn(val ticket: Int, val row: Int, val column: Int)
 }
