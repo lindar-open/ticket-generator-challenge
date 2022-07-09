9a99a806db50547622d069628a3b7310b3ad97b3
Attila Palfi <attila.palfi.92@gmail.com>
2022-07-09 15:54:45 +0200

    added performance test

---
+++ src/test/kotlin/com/attilapalfi/lindar/bingoticketgenerator/performancetest/PerformanceTest.kt
@@ -0,0 +1,55 @@
+package com.attilapalfi.lindar.bingoticketgenerator.performancetest
+
+import com.attilapalfi.lindar.bingoticketgenerator.random.SimpleRandomProvider
+import com.attilapalfi.lindar.bingoticketgenerator.ticketgenerator.ArrayBasedTicketGenerator
+import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+class PerformanceTest {
+
+    private val randomProvider = SimpleRandomProvider()
+
+    @Test
+    fun `measure time for 100_000 strip generation`() {
+        warmUpJvm()
+
+        val strips = 100_000
+        val startTime = System.currentTimeMillis()
+        for (i in 0 until strips) {
+            ArrayBasedTicketGenerator(randomProvider).generateTickets()
+        }
+        val executionTime = System.currentTimeMillis() - startTime
+        println("Generated $strips strips in $executionTime ms.")
+    }
+
+    @Test
+    fun `measure time for 100_000 strip generation in parallel`() {
+        warmUpJvm()
+
+        val strips = 100_000
+        val startTime = System.currentTimeMillis()
+        val threads = Runtime.getRuntime().availableProcessors()
+        val executorService = Executors.newFixedThreadPool(threads)
+        val chunkSize = strips / threads
+        val futures = mutableListOf<Future<*>>()
+        for (i in 0 until threads) {
+            val future = executorService.submit {
+                for (j in 0..chunkSize) {
+                    ArrayBasedTicketGenerator(randomProvider).generateTickets()
+                }
+            }
+            futures.add(future)
+        }
+        futures.forEach { it.get() }
+        executorService.shutdown()
+        val executionTime = System.currentTimeMillis() - startTime
+        println("Generated $strips strips in parallel in $executionTime ms.")
+    }
+
+    private fun warmUpJvm() {
+        for (i in 0 until 1000) {
+            ArrayBasedTicketGenerator(randomProvider).generateTickets()
+        }
+    }
+}
